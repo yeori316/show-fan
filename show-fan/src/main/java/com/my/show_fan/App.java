5aec7d308162c2ef33a71ac0634ec30c159e269e
hongsikcho <jjdd9758@naver.com>
2023-09-26 11:36:24 +0900

    build : maven프로젝트 생성하다

---
+++ show-fan/src/main/java/com/my/show_fan/App.java
@@ -0,0 +1,13 @@
+package com.my.show_fan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
