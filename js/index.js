abc5d0d0ccff6099d4635310b197f478c55a7ad1
leejaeseong11 <easeon78@gmail.com>
2023-09-27 17:39:16 +0900

    feat: develop footer

--- js/index.js
+++ js/index.js
@@ -3,4 +3,5 @@ import { handleXhttps } from './util.js';
 $(() => {
   handleXhttps('GET', '../html/header.html', $('header'));
   handleXhttps('GET', '../html/navigation.html', $('nav'));
+  handleXhttps('GET', '../html/footer.html', $('footer'));
 });
