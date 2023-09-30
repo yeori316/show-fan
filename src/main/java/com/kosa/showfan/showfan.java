4dafa0d5fab4afc396ae093cd7e46366c07b152d
dozy <78740368+yeori316@users.noreply.github.com>
2023-09-30 18:28:18 +0900

    back init

---
+++ src/main/java/com/kosa/showfan/showfan.java
@@ -0,0 +1,10 @@
+package com.kosa.showfan;
+
+public class showfan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello show-fan!");
+	}
+
+}
