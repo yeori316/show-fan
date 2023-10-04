614d2d0bf13d43412d9ddcdf917f956bed7a0756
KANU <65529758+KANU896@users.noreply.github.com>
2023-10-04 12:24:33 +0900

    feat: connect from login.html to index.html (js)

--- js/index.js
+++ js/index.js
@@ -4,4 +4,10 @@ $(() => {
   handleXhttps('GET', '../html/header.html', $('header'));
   handleXhttps('GET', '../html/navigation.html', $('nav'));
   handleXhttps('GET', '../html/footer.html', $('footer'));
+
+
+  $('body').on('click', '.header-menu-link[href="login.html"]', function(e) {
+    e.preventDefault();
+    handleXhttps('GET', '../html/login.html', $('main'));
+  });
 });
