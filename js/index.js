a4eff1f0785279b2b7fdc2a45b63750833bcea1a
KANU <65529758+KANU896@users.noreply.github.com>
2023-10-04 12:47:59 +0900

    feat: develop modify

--- js/index.js
+++ js/index.js
@@ -7,4 +7,11 @@ $(() => {
 
   // if 마이페이지 link clicked
   handleXhttps('GET', '../html/mypage.html', $('main'));
+
+  // 내 정보 수정 클릭 시
+  $('body').on('click', '.mypage-main-text[href="modify.html"]', function(e) {
+    e.preventDefault();
+    handleXhttps('GET', '../html/modify.html', $('main'));
+  });
+
 });
