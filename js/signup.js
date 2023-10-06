89f349bb7a4c2702ac92232e1dc225fc1475ce81
KANU <65529758+KANU896@users.noreply.github.com>
2023-10-06 12:33:35 +0900

    fix: signup 오류해결
    1. 인증버튼 클릭시 타이머 여러번 작동하는 문제
    2. 타이머 시간 5분으로 수정
    3. 부트스트랩 CDN링크로 연결

--- js/signup.js
+++ js/signup.js
@@ -9,35 +9,46 @@ $(() => {
   $EmailChkObj.hide();
   $btEmailChkObj.hide();
 
+  let timerRunning = false;  // 타이머가 실행 중인지 여부를 나타내는 변수
+
   $btCertifiedObj.click(() => {
+    if (timerRunning) {
+      return;  // 타이머가 이미 실행 중이면 클릭을 무시
+    }
+  
+    timerRunning = true;  // 타이머 시작
+  
     $EmailChkObj.show();
     $btEmailChkObj.show();
-    let minutes = 1;
+    let minutes = 5;
     let seconds = 0;
-
+  
     function updateTimer() {
       if (minutes === 0 && seconds === 0) {
         $timerElement.text("시간 초과");
         $EmailChkObj.hide();
+        timerRunning = false;  // 타이머 종료
         return;
       }
-
+  
       if (seconds === 0) {
         minutes--;
         seconds = 59;
       } else {
         seconds--;
       }
-
+  
       let formattedTime = `${String(minutes).padStart(2, "0")}:${String(
         seconds
       ).padStart(2, "0")}`;
       $timerElement.text(formattedTime);
-
+  
       setTimeout(updateTimer, 1000);
     }
-
+  
     updateTimer();
   });
+  
+
   
 });
