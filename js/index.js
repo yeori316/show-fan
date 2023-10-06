0af0f0fff8d38a2ab35eb1e88eaf9a218a9596b7
hongsikcho <jjdd9758@naver.com>
2023-10-06 15:31:24 +0900

    fix : 첫화면 마이페이지에서 메인화면으로 변경

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(() => {
   handleXhttps("GET", "../html/footer.html", $("footer"));
 
   // if 마이페이지 link clicked
-  handleXhttps("GET", "../html/mypage.html", $("main"));
+  handleXhttps("GET", "../html/main.html", $("main"));
 
   // 내 정보 수정 클릭 시
   $("body").on("click", '.mypage-main-text[href="modify.html"]', function (e) {
