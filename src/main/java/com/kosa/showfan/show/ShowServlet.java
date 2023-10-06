c36522f4b70f7429b1c306312e8fa383024b0fbf
hongsikcho <76928632+hongsikcho@users.noreply.github.com>
2023-10-06 17:00:24 +0900

    Update ShowServlet.java

--- src/main/java/com/kosa/showfan/show/ShowServlet.java
+++ src/main/java/com/kosa/showfan/show/ShowServlet.java
@@ -25,7 +25,7 @@ public class ShowServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.setHeader("Access-Control-Allow-Origin", "http://192.168.1.37:5501");
+		response.setHeader("Access-Control-Allow-Origin", "/*");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setContentType("application/json;charset=utf-8");
 		
