40e703cdcc55b7f33a357cf8038801f8236774a7
dozy <78740368+yeori316@users.noreply.github.com>
2023-10-11 22:23:43 +0900

    feat: search filter

--- js/search.js
+++ js/search.js
@@ -1,8 +1,17 @@
 import { handleXhttps, backURL } from "./util.js";
 
 $(() => {
+  const theaterId = 1;
+  const musicalId = 2;
+  const classicId = 3;
+  const concertId = 4;
+  const festivalId = 5;
+
   const queryStr = location.search.substring(1); // v=value
   const value = queryStr.substring(2); // value
+
+  var showCnt;
+  var showList;
 
   handleXhttps("GET", "../html/header.html", $("header"));
   handleXhttps("GET", "../html/navigation.html", $("nav"));
@@ -53,77 +62,50 @@ $(() => {
     calendar.render();
   });
 
-  var showCnt;
-  var showList;
+  // 장르 전체 선택
+  $("#genreAll").click((e) => {
+    allChkHandler($("#genreAll"), "genre");
+  });
+  // 장르 전체 선택 끝
+
+  // 공연상태 전체 선택
+  $("#statusAll").click((e) => {
+    allChkHandler($("#statusAll"), "status");
+  });
+  // 공연상태 전체 선택 끝
+
+  // 지역 전체 선택
+  $("#localAll").click((e) => {
+    allChkHandler($("#localAll"), "local");
+  });
+  // 지역 전체 선택 끝
+
+  // 체크박스 필터
+  $(".form-check-input").click((e) => {
+    showListFilterHandler(showList);
+  });
+  // 체크박스 필터 끝
+
+  // 검색 기능 호출
+  ajaxHandler(value);
+  // 검색 기능 호출 끝
 
   // 검색 기능
   function ajaxHandler(value) {
-    // ajax 요청
     $.ajax({
       url: `${backURL}/search?${queryStr}`,
       method: "GET",
       success: (responseJSONObj) => {
         showCnt = responseJSONObj.showCnt;
         showList = responseJSONObj.show;
-
-        // if (showCnt != 0) {
         showListHandler(showCnt, showList);
-        // } else {
-        //   $("#showCardList").html(
-        //     '<div id="search-result-count" class="container text-right"><b>검색된 공연이 없습니다.</b></div>'
-        //   );
-        // }
       },
       error: (xhr, textStatus) => {
         alert("err: " + xhr.status);
       },
     });
-    // ajax 요청 끝
-
-    // 장르 전체 선택
-    $("#genreAll").click((e) => {
-      allChkHandler($("#genreAll"), "genre");
-    });
-    // 장르 전체 선택 끝
-
-    // 공연상태 전체 선택
-    $("#statusAll").click((e) => {
-      allChkHandler($("#statusAll"), "status");
-    });
-    // 공연상태 전체 선택 끝
-
-    // 지역 전체 선택
-    $("#localAll").click((e) => {
-      allChkHandler($("#localAll"), "local");
-    });
-    // 지역 전체 선택 끝
   }
   // 검색 기능 종료
-
-  // 검색 기능 호출
-  ajaxHandler(value);
-  // 검색 기능 호출 끝
-
-  // $("#theater").prop("checked", true);
-  // $("#musical").prop("checked", true);
-  // $("#classic").prop("checked", true);
-  // $("#concert").prop("checked", true);
-  // $("#festival").prop("checked", true);
-
-  $("#theater").click((e) => {
-    if ($("#theater").is(":checked")) {
-      showListHandler(showCnt, showList);
-    } else {
-      var notTheaterShowList = showList.filter(function (show) {
-        return show.genreId != 1;
-      });
-      console.log(notTheaterShowList.length);
-
-      $("#search-result-container").html(
-        showListHandler(notTheaterShowList.length, notTheaterShowList)
-      );
-    }
-  });
 
   // 필터 전체 체크 함수 시작
   function allChkHandler($allChk, name) {
@@ -134,6 +116,169 @@ $(() => {
     }
   }
   // 필터 전체 체크 함수 끝
+
+  // show list 장르체크 필터
+  function showListFilterGenreHandler(showList, $filterChk, genreId) {
+    var showFilterList = showList;
+
+    if (!$filterChk.is(":checked")) {
+      showFilterList = showFilterList.filter(function (show) {
+        return show.genreId != genreId;
+      });
+    }
+    return showFilterList;
+  }
+  // show list 장르체크 필터 끝
+
+  // show list 상태체크 필터
+  function showListFilterStatusHandler(showList, $filterChk, status) {
+    var showFilterList = showList;
+
+    if (!$filterChk.is(":checked")) {
+      showFilterList = showFilterList.filter(function (show) {
+        return show.showStatus != status;
+      });
+    }
+    return showFilterList;
+  }
+  // show list 상태체크 필터 끝
+
+  // show list 지역체크 필터
+  function showListFilterLocalHandler(showList, $filterChk, local) {
+    var showFilterList = showList;
+
+    if (!$filterChk.is(":checked")) {
+      showFilterList = showFilterList.filter(function (show) {
+        return show.showAddress.indexOf(local) == -1;
+      });
+    }
+    return showFilterList;
+  }
+  // show list 지역체크 필터 끝
+
+  // show list 체크 필터
+  function showListFilterHandler(showList) {
+    var showFilterList;
+
+    // show list 장르체크 필터
+    showFilterList = showListFilterGenreHandler(
+      showList,
+      $("#theater"),
+      theaterId
+    );
+    showFilterList = showListFilterGenreHandler(
+      showFilterList,
+      $("#musical"),
+      musicalId
+    );
+    showFilterList = showListFilterGenreHandler(
+      showFilterList,
+      $("#classic"),
+      classicId
+    );
+    showFilterList = showListFilterGenreHandler(
+      showFilterList,
+      $("#concert"),
+      concertId
+    );
+    showFilterList = showListFilterGenreHandler(
+      showFilterList,
+      $("#festival"),
+      festivalId
+    );
+    // show list 장르체크 필터 끝
+
+    // show list 상태체크 필터
+    showFilterList = showListFilterStatusHandler(
+      showFilterList,
+      $("#coming"),
+      "공연예정"
+    );
+    showFilterList = showListFilterStatusHandler(
+      showFilterList,
+      $("#during"),
+      "공연중"
+    );
+    showFilterList = showListFilterStatusHandler(
+      showFilterList,
+      $("#end"),
+      "공연완료"
+    );
+    // show list 상태체크 필터 끝
+
+    // show list 지역체크 필터
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#seoul"),
+      "서울"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#incheon"),
+      "인천"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#daejeon"),
+      "대전"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#daegu"),
+      "대구"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#gwangju"),
+      "광주"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#ulsan"),
+      "울산"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#busan"),
+      "부산"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#gyeonggi"),
+      "경기"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#gangwon"),
+      "강원"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#gyeongsang"),
+      "경상"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#chungcheong"),
+      "충청"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#jeolla"),
+      "전라"
+    );
+    showFilterList = showListFilterLocalHandler(
+      showFilterList,
+      $("#jeju"),
+      "제주"
+    );
+    // show list 지역체크 필터
+
+    $("#search-result-container").html(
+      showListHandler(showFilterList.length, showFilterList)
+    );
+  }
+  // show list 체크 필터 끝
 
   // show list handler
   function showListHandler(showCnt, showList) {
@@ -188,22 +333,30 @@ $(() => {
 
         if (showAddress.indexOf("서울") != -1) {
           $("#seoul").prop("checked", true);
-        } else if (showAddress.indexOf("인천")) {
+        } else if (showAddress.indexOf("인천") != -1) {
           $("#incheon").prop("checked", true);
-        } else if (showAddress.indexOf("대전")) {
+        } else if (showAddress.indexOf("대전") != -1) {
           $("#daejeon").prop("checked", true);
-        } else if (showAddress.indexOf("대구")) {
+        } else if (showAddress.indexOf("대구") != -1) {
           $("#daegu").prop("checked", true);
-        } else if (showAddress.indexOf("광주")) {
+        } else if (showAddress.indexOf("광주") != -1) {
           $("#gwangju").prop("checked", true);
-        } else if (showAddress.indexOf("울산")) {
+        } else if (showAddress.indexOf("울산") != -1) {
           $("#ulsan").prop("checked", true);
-        } else if (showAddress.indexOf("부산")) {
+        } else if (showAddress.indexOf("부산") != -1) {
           $("#busan").prop("checked", true);
-        } else if (showAddress.indexOf("제주")) {
+        } else if (showAddress.indexOf("경기") != -1) {
+          $("#gyeonggi").prop("checked", true);
+        } else if (showAddress.indexOf("강원") != -1) {
+          $("#gangwon").prop("checked", true);
+        } else if (showAddress.indexOf("경상") != -1) {
+          $("#gyeongsang").prop("checked", true);
+        } else if (showAddress.indexOf("충청") != -1) {
+          $("#chungcheong").prop("checked", true);
+        } else if (showAddress.indexOf("전라") != -1) {
+          $("#jeolla").prop("checked", true);
+        } else if (showAddress.indexOf("제주") != -1) {
           $("#jeju").prop("checked", true);
-        } else {
-          $("#etc").prop("checked", true);
         }
 
         $copyShow.find("img").attr("src", showImage).attr("alt", showName);
@@ -226,88 +379,4 @@ $(() => {
     }
   }
   // show list handler 끝
-
-  // show list filter genre handler 시작
-  function showListFilterGenreHandler(showCnt, showList, genre) {
-    const $resultCnt = $("#search-result-count > b");
-    $resultCnt.html(`검색 결과 (${showCnt})`);
-
-    const $originShow = $("div.col").first();
-    $originShow.siblings().remove();
-    $originShow.show();
-
-    $(showList).each((index, s) => {
-      const $copyShow = $originShow.clone();
-
-      const showId = s.showId;
-      // const showImage = s.showPoster;
-      const showImage =
-        "https://showfan.s3.ap-northeast-2.amazonaws.com/PF154190.jpg";
-      const genreId = s.genreId;
-      const showName = s.showName;
-      const showVenues = s.showVenues;
-      const showStartDay = s.showStartDay;
-      const showEndDay = s.showEndDay;
-      const showAddress = s.showAddress;
-      const showStatus = s.showStatus;
-      const reviewCnt = s.reviewCnt;
-      const gradeAvg = s.gradeAvg;
-
-      if (genreId == 1) {
-        $("#theater").prop("checked", true);
-      } else if (genreId == 2) {
-        $("#musical").prop("checked", true);
-      } else if (genreId == 3) {
-        $("#classic").prop("checked", true);
-      } else if (genreId == 4) {
-        $("#concert").prop("checked", true);
-      } else if (genreId == 5) {
-        $("#festival").prop("checked", true);
-      }
-
-      if (showStatus == "공연예정") {
-        $("#coming").prop("checked", true);
-      } else if (showStatus == "공연중") {
-        $("#during").prop("checked", true);
-      } else if (showStatus == "공연완료") {
-        $("#end").prop("checked", true);
-      }
-
-      if (showAddress.indexOf("서울") != -1) {
-        $("#seoul").prop("checked", true);
-      } else if (showAddress.indexOf("인천")) {
-        $("#incheon").prop("checked", true);
-      } else if (showAddress.indexOf("대전")) {
-        $("#daejeon").prop("checked", true);
-      } else if (showAddress.indexOf("대구")) {
-        $("#daegu").prop("checked", true);
-      } else if (showAddress.indexOf("광주")) {
-        $("#gwangju").prop("checked", true);
-      } else if (showAddress.indexOf("울산")) {
-        $("#ulsan").prop("checked", true);
-      } else if (showAddress.indexOf("부산")) {
-        $("#busan").prop("checked", true);
-      } else if (showAddress.indexOf("제주")) {
-        $("#jeju").prop("checked", true);
-      } else {
-        $("#etc").prop("checked", true);
-      }
-
-      $copyShow.find("img").attr("src", showImage).attr("alt", showName);
-      if (showStatus == "공연완료") {
-        $copyShow.find(".status > b").html("공연종료");
-      } else {
-        $copyShow.find(".status > b").html(showStatus);
-      }
-      $copyShow.find(".card-title").html(showName);
-      $copyShow.find("#period").html(showStartDay + " ~ " + showEndDay);
-      $copyShow.find("#venues").html(showVenues);
-      $copyShow.find("#grade").html("별 " + gradeAvg + " (" + reviewCnt + ")");
-
-      $("#showCardList").append($copyShow);
-    });
-
-    $originShow.hide();
-  }
-  // show list filter genre handler 끝
 });
