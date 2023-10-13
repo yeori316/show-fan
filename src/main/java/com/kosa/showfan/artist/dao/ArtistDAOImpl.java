34b994535cfb6b70f031b3dd53245607f0ac7a0c
leejaeseong11 <easeon78@gmail.com>
2023-10-13 19:40:16 +0900

    feat: artist search

--- src/main/java/com/kosa/showfan/artist/dao/ArtistDAOImpl.java
+++ src/main/java/com/kosa/showfan/artist/dao/ArtistDAOImpl.java
@@ -1,5 +1,49 @@
 package com.kosa.showfan.artist.dao;
 
+import com.kosa.showfan.artist.dto.ArtistDTO;
+import com.kosa.showfan.exception.FindException;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+
 public class ArtistDAOImpl implements ArtistDAO {
 
+    private static final ArtistDAO artistDAO = new ArtistDAOImpl();
+
+    public static ArtistDAO getInstance() {
+        return artistDAO;
+    }
+
+    private SqlSessionFactory sqlSessionFactory;
+
+    public ArtistDAOImpl() {
+        String resource = "com/kosa/showfan/sql/mybatis-config.xml";
+        InputStream inputStream;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public ArtistDTO selectById(Long artistId) throws FindException {
+        SqlSession session = null;
+        try {
+            session = sqlSessionFactory.openSession();
+            ArtistDTO artistDTO = session.selectOne("com.kosa.artist.ArtistMapper.selectById", artistId);
+            return artistDTO;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new FindException("아티스트 검색에 실패했습니다");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 }
