c30548c73a60bb00c5787b3337138ab1dc093dc3
aeokseung <oksh3012@naver.com>
2023-10-13 16:02:23 +0900

    Fix: memberId select 오류 수정

--- src/main/java/com/kosa/showfan/controller/DispatcherServlet.java
+++ src/main/java/com/kosa/showfan/controller/DispatcherServlet.java
@@ -17,10 +17,10 @@ public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private String envFileName = "controller.properties";
 	private Properties env;
-	
-    public DispatcherServlet() {
-    	super();
-    }
+
+	public DispatcherServlet() {
+		super();
+	}
 
 	@Override
 	public void init() throws ServletException {
@@ -28,37 +28,40 @@ public class DispatcherServlet extends HttpServlet {
 		env = new Properties();
 		ServletContext sc = this.getServletContext();
 		String realPath = sc.getRealPath("WEB-INF\\classes\\" + envFileName);
-//		System.out.println(">>>" + realPath);
+		System.out.println(">>>" + realPath);
 
-    	try {
+		try {
 			env.load(new FileInputStream(realPath));
+			String path = (String) env.get("/memberreview");
+			System.out.println(path);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("request.getServletPath()=" + request.getServletPath());
-		
+
 //		if (request.getServletPath().equals("/productjson")) {
 //			ProductJsonController control = new ProductJsonControler();
 //			control.execute(request, response);
 //		}
-		
-		response.setHeader("Access-Control-Allow-Origin", "http://192.168.1.103:5501");//"http://127.0.0.1:5500");
+
+		response.setHeader("Access-Control-Allow-Origin", "*");// "http://127.0.0.1:5500");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
-		
-		//DispatcherServlet의service()입니다
+
+		// DispatcherServlet의service()입니다
 		String className = env.getProperty(request.getServletPath());
 //		className = "com.kosa.showfan.show.controller.ShowServlet";
-		
+
 		try {
-			Class<?> clazz = Class.forName(className); //클래스이름에 해당하는 .class파일 찾아서 JVM으로 로드
+			Class<?> clazz = Class.forName(className); // 클래스이름에 해당하는 .class파일 찾아서 JVM으로 로드
 			Controller controller;
 			try {
 				Method method = clazz.getMethod("getInstance");
-				controller = (Controller) method.invoke(null); //static인 getInstance()메서드호출
-			}catch(NoSuchMethodException e) {			
+				controller = (Controller) method.invoke(null); // static인 getInstance()메서드호출
+			} catch (NoSuchMethodException e) {
 				controller = (Controller) clazz.getDeclaredConstructor().newInstance();
 			}
 			controller.execute(request, response);
