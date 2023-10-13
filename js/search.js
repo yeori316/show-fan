0ec6a9afeb0ca0e0b860027ee177fe079f25b2d7
dozy <78740368+yeori316@users.noreply.github.com>
2023-10-13 21:06:08 +0900

    fix:search

--- js/search.js
+++ js/search.js
@@ -304,7 +304,7 @@ $(() => {
     if (showCnt == 0) {
       $("#search-result-count > b").html(`검색 결과 (${showCnt})`);
       $("#showCardList").html(
-        '<div class="col"><div class="card h-100"><img class="card-img-top" src="https://showfan.s3.ap-northeast-2.amazonaws.com/PF154190.jpg" alt="..."/><div class="status"><b>공연중</b></div><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text"><div id="period">2023.01.01 ~ 2023.01.01</div><div id="venues">예술의 전당</div><br><div id="grade">평점 3.5 (리뷰갯수)</div></p></div></div></div><div id="search-result-count" class="container text-right"><b>검색된 공연이 없습니다.</b></div>'
+        '<div class="col"><a><div class="card h-100"><img class="card-img-top" src="https://showfan.s3.ap-northeast-2.amazonaws.com/PF154190.jpg" alt="..."/><div class="status"><b>공연중</b></div><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text"><div id="period">2023.01.01 ~ 2023.01.01</div><div id="venues">예술의 전당</div><br><div id="grade">평점 3.5 (리뷰갯수)</div></p></div></div></a></div><div id="search-result-count" class="container text-right"><b>검색된 공연이 없습니다.</b></div>'
       );
       $("div.col").first().hide();
     } else {
