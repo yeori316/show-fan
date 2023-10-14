0893d592b657e2c8c85ec15a71c5e8b8105ac3e0
leejaeseong11 <easeon78@gmail.com>
2023-10-14 17:52:44 +0900

    fix: member review select

--- src/main/java/com/kosa/showfan/review/controller/MemberReviewController.java
+++ src/main/java/com/kosa/showfan/review/controller/MemberReviewController.java
@@ -1,36 +1,33 @@
 package com.kosa.showfan.review.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
+import com.google.gson.Gson;
+import com.kosa.showfan.review.dto.ReviewShowResponseDTO;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.gson.Gson;
-import com.kosa.showfan.review.dto.ReviewResponseDTO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class MemberReviewController extends ReviewController {
 
-	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("??");
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setContentType("application/json;charset=utf-8");
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setContentType("application/json;charset=utf-8");
 
-		System.out.println("memberReviewController");
-		PrintWriter out = response.getWriter();
-		Long memberId = Long.parseLong(request.getParameter("memberId"));
+        PrintWriter out = response.getWriter();
+        Long memberId = Long.parseLong(request.getParameter("memberId"));
 
-		try {
-			List<ReviewResponseDTO> result = service.getSelectByMemberId(memberId);
-			Gson gson = new Gson();
-			String jsonResult = gson.toJson(result);
-			out.print(jsonResult);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            List<ReviewShowResponseDTO> result = service.getSelectByMemberId(memberId);
+            Gson gson = new Gson();
+            String jsonResult = gson.toJson(result);
+            out.print(jsonResult);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/kosa/showfan/review/service/ReviewService.java
+++ src/main/java/com/kosa/showfan/review/service/ReviewService.java
@@ -1,7 +1,5 @@
 package com.kosa.showfan.review.service;
 
-import java.util.List;
-
 import com.kosa.showfan.exception.AddException;
 import com.kosa.showfan.exception.FindException;
 import com.kosa.showfan.exception.ModifyException;
@@ -10,40 +8,43 @@ import com.kosa.showfan.review.dao.ReviewDAO;
 import com.kosa.showfan.review.dao.ReviewDAOImpl;
 import com.kosa.showfan.review.dto.ReviewDTO;
 import com.kosa.showfan.review.dto.ReviewResponseDTO;
+import com.kosa.showfan.review.dto.ReviewShowResponseDTO;
+
+import java.util.List;
 
 public class ReviewService {
-	private static final ReviewService service = new ReviewService();
-	private ReviewDAO reviewDAO = ReviewDAOImpl.getInstance();
+    private static final ReviewService service = new ReviewService();
+    private ReviewDAO reviewDAO = ReviewDAOImpl.getInstance();
 
-	public static ReviewService getInstance() {
-		return service;
-	}
+    public static ReviewService getInstance() {
+        return service;
+    }
 
-	public List<ReviewResponseDTO> getSelectByShowId(String showId) throws FindException {
-		return reviewDAO.selectByShowId(showId);
-	}
+    public List<ReviewResponseDTO> getSelectByShowId(String showId) throws FindException {
+        return reviewDAO.selectByShowId(showId);
+    }
 
-	public List<ReviewResponseDTO> getSelectByMemberId(Long memberId) throws FindException {
-		return reviewDAO.selectByMemberId(memberId);
-	}
+    public List<ReviewShowResponseDTO> getSelectByMemberId(Long memberId) throws FindException {
+        return reviewDAO.selectByMemberId(memberId);
+    }
 
-	public void insertReview(ReviewDTO reviewDTO) throws AddException {
-		reviewDAO.insertReview(reviewDTO);
-	}
+    public void insertReview(ReviewDTO reviewDTO) throws AddException {
+        reviewDAO.insertReview(reviewDTO);
+    }
 
-	public void updateReview(ReviewDTO reviewDTO) {
-		try {
-			reviewDAO.updateReview(reviewDTO);
-		} catch (ModifyException e) {
-			e.printStackTrace();
-		}
-	}
+    public void updateReview(ReviewDTO reviewDTO) {
+        try {
+            reviewDAO.updateReview(reviewDTO);
+        } catch (ModifyException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void deleteReview(Long reviewId) {
-		try {
-			reviewDAO.deleteReview(reviewId);
-		} catch (RemoveException e) {
-			e.printStackTrace();
-		}
-	}
+    public void deleteReview(Long reviewId) {
+        try {
+            reviewDAO.deleteReview(reviewId);
+        } catch (RemoveException e) {
+            e.printStackTrace();
+        }
+    }
 }
