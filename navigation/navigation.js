a4c0480d909dfb29969614f4baf3a78f43725a39
leejaeseong11 <easeon78@gmail.com>
2023-10-16 02:42:42 +0900

    feat: review in detail page

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,30 +1,31 @@
-document.querySelector(".classic").addEventListener("click", classic);
-document.querySelector(".festival").addEventListener("click", festival);
-document.querySelector(".concert").addEventListener("click", concert);
-document.querySelector(".show").addEventListener("click", show);
-document.querySelector(".musical").addEventListener("click", musical);
-document.querySelector(".all").addEventListener("click", all);
+document.querySelector('.classic').addEventListener('click', classic);
+document.querySelector('.festival').addEventListener('click', festival);
+document.querySelector('.concert').addEventListener('click', concert);
+document.querySelector('.show').addEventListener('click', show);
+document.querySelector('.musical').addEventListener('click', musical);
+document.querySelector('.all').addEventListener('click', all);
 
 function classic() {
-  window.localStorage.setItem("show_genre", 3);
-  location.href = "../html/genre.html";
+  console.log('clasic');
+  window.localStorage.setItem('show_genre', 3);
+  location.href = '../genre';
 }
 function festival() {
-  window.localStorage.setItem("show_genre", 5);
-  location.href = "../html/genre.html";
+  window.localStorage.setItem('show_genre', 5);
+  location.href = '../genre';
 }
 function musical() {
-  window.localStorage.setItem("show_genre", 2);
-  location.href = "../html/genre.html";
+  window.localStorage.setItem('show_genre', 2);
+  location.href = '../genre';
 }
 function concert() {
-  window.localStorage.setItem("show_genre", 4);
-  location.href = "../html/genre.html";
+  window.localStorage.setItem('show_genre', 4);
+  location.href = '../genre';
 }
 function show() {
-  window.localStorage.setItem("show_genre", 1);
-  location.href = "../html/genre.html";
+  window.localStorage.setItem('show_genre', 1);
+  location.href = '../genre';
 }
 function all() {
-  location.href = "../index.html";
+  location.href = '../index.html';
 }
