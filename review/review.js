803da5a521ba5674057d4aa1c889c89e221081ab
hongsikcho <jjdd9758@naver.com>
2023-10-16 18:03:58 +0900

    feat : 출연진 정보없을때 출력안됨

--- review/review.js
+++ review/review.js
@@ -1,20 +1,20 @@
-import { backURL } from '../util/util.js';
+import { backURL } from "../util/util.js";
 
 $(() => {
   function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+    if (parts.length === 2) return parts.pop().split(";").shift();
   }
 
-  const loginCookie = getCookie('loginCookie');
+  const loginCookie = getCookie("loginCookie");
 
   let showId;
   let memberId;
   $.ajax({
-    type: 'get',
-    url: backURL + `/findshow?showname=${window.localStorage.getItem('param')}`,
-    dataType: 'json',
+    type: "get",
+    url: backURL + `/findshow?showname=${window.localStorage.getItem("param")}`,
+    dataType: "json",
     success: function (responseShowId) {
       showId = responseShowId;
       //--reviewselect start--
@@ -35,7 +35,7 @@ $(() => {
       };
       $.ajax({
         url: `${backURL}/selectreview`,
-        method: 'GET',
+        method: "GET",
         data: `showId=${data.showId}`,
         success: (responseJSONObj) => {
           displayReviews(responseJSONObj);
@@ -46,8 +46,8 @@ $(() => {
   });
 
   $.ajax({
-    url: backURL + '/member',
-    method: 'GET',
+    url: backURL + "/member",
+    method: "GET",
     data: `email=${loginCookie}`,
     success: (memberResponseText) => {
       if (memberResponseText != null) memberId = memberResponseText.memberId;
@@ -55,38 +55,38 @@ $(() => {
   });
   //--모달 시작--
   // 모달 요소 가져오기
-  var modal = document.getElementById('myModal');
+  var modal = document.getElementById("myModal");
 
   // 모달 닫기 버튼 가져오기
-  var closeModalBtn = document.getElementById('closeModalBtn');
+  var closeModalBtn = document.getElementById("closeModalBtn");
 
   // 모달 등록 버튼 가져오기
-  var submitbutton = document.getElementById('submitbutton');
+  var submitbutton = document.getElementById("submitbutton");
 
   // 모달 닫기 버튼 클릭 시 모달을 숨김
   closeModalBtn.onclick = function () {
-    modal.style.display = 'none';
+    modal.style.display = "none";
   };
 
   // 모달 영역 외부 클릭 시 모달을 숨김
   window.onclick = function (event) {
     if (event.target == modal) {
-      modal.style.display = 'none';
+      modal.style.display = "none";
     }
   };
   // 모달 등록 버튼 클릭 시 모달을 숨김
   submitbutton.onclick = function () {
-    modal.style.display = 'none';
+    modal.style.display = "none";
     if (loginCookie == null) {
-      alert('로그인 후 리뷰 쓰기가 가능합니다');
+      alert("로그인 후 리뷰 쓰기가 가능합니다");
       return;
     }
     $.ajax({
-      method: 'GET',
+      method: "GET",
       url:
         backURL +
-        `/insertreview?seatId=${$('#seat').val()}&reviewContent=${$(
-          '#reviewContent'
+        `/insertreview?seatId=${$("#seat").val()}&reviewContent=${$(
+          "#reviewContent"
         ).val()}&reviewGrade=${$(
           "input[name='reviewGrade']:checked"
         ).val()}&showId=${showId}&memberId=${memberId}`,
@@ -100,8 +100,8 @@ $(() => {
     });
   };
   // 모달을 숨김
-  var modal = document.getElementById('myModal');
-  modal.style.display = 'none';
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
   //--모달 끝--
 });
 
@@ -109,7 +109,7 @@ var reviews = [];
 var replies = [];
 // 리뷰 데이터를 화면에 출력하는 함수
 function displayReviews(reviews) {
-  console.log('reviews= ' + reviews);
+  console.log("reviews= " + reviews);
   console.log(reviews);
   for (var i = 0; i < reviews.length; i++) {
     const $reviewContainer = $(
@@ -124,11 +124,11 @@ function displayReviews(reviews) {
     $reviewContainer.append(`<div>${reviews[i].seatName}</div>`);
     $reviewContainer.append(`<div>${reviews[i].reviewContent}</div>`);
     $reviewContainer.append(`<div>_________________________________</div>`);
-    $('#showdetail-review').append($reviewContainer);
+    $("#showdetail-review").append($reviewContainer);
   }
 }
 function displayReplies(replies) {
-  console.log('replies= ' + replies);
+  console.log("replies= " + replies);
   console.log(replies);
   for (var i = 0; i < replies.length; i++) {
     const $reviewContainer = $(
@@ -136,27 +136,27 @@ function displayReplies(replies) {
     );
     const $replyContainer = $('<div class="showdetail-reply-container"></div>');
     $replyContainer.append(`<div>${replies[i].replyContent}</div>`);
-    $('#showdetail-reply').append($replyContainer);
+    $("#showdetail-reply").append($replyContainer);
   }
 }
 // 리뷰 쓰기
-$('#create-review').click(() => {
+$("#create-review").click(() => {
   initPopup();
   // 리뷰 쓰기 버튼 클릭 시 모달을 보이게 설정
-  var modal = document.getElementById('myModal');
-  modal.style.display = 'block';
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
   let showName;
-  $('.review-form > h2').text(
-    (showName = decodeURI(window.location.search).replace('?showname=', ''))
+  $(".review-form > h2").text(
+    (showName = decodeURI(window.location.search).replace("?showname=", ""))
   );
   $.ajax({
-    type: 'get',
+    type: "get",
     url: backURL + `/findshow?showname=${showName}`,
-    dataType: 'json',
+    dataType: "json",
     success: function (responseShowId) {
       $.ajax({
-        url: backURL + '/showdetail',
-        method: 'GET',
+        url: backURL + "/showdetail",
+        method: "GET",
         data: `showId=${responseShowId}`,
         success: (showReviewResponse) => {
           let seatName = {};
@@ -164,13 +164,13 @@ $('#create-review').click(() => {
             if (seatName[showReview.seatId] == undefined) {
               seatName[showReview.seatId] =
                 showReview.seatName +
-                ' (' +
+                " (" +
                 showReview.seatPrice.toLocaleString() +
-                '원)';
+                "원)";
             }
           });
           Object.keys(seatName).forEach((seatId) => {
-            $('#seat').append(
+            $("#seat").append(
               `<option value=${seatId}>${seatName[seatId]}</option>`
             );
           });
@@ -181,9 +181,9 @@ $('#create-review').click(() => {
 });
 
 function initPopup() {
-  $('.review-form > h2').empty();
-  $('#seat').empty();
-  $('#reviewContent').empty();
+  $(".review-form > h2").empty();
+  $("#seat").empty();
+  $("#reviewContent").empty();
 }
 
 //----------리뷰 end---
